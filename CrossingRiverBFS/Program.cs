0e9867956a0eb83ab9cc96b40600d75a0e3cc4bc
paulorcmendes <mendesdpn@gmail.com>
2017-10-26 00:17:24 -0300

    Entrada de dados

--- CrossingRiverBFS/Program.cs
+++ CrossingRiverBFS/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 namespace CrossingRiverBFS
 {
+
     public enum label {
         FARMER = 0,
         GRAINS = 1,
@@ -12,6 +13,27 @@ namespace CrossingRiverBFS
     }
     class Program
     {
+        public static bool[] ReadState() {
+            string[] names = { "Farmer", "Grains", "Chicken", "Fox" };
+            bool[] state = new bool[4];
+            string aux;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Side of {0}: ", names[i]);
+                aux = Console.ReadLine();
+                while (aux != "L" && aux != "R")
+                {
+                    Console.Beep();
+                    Console.WriteLine("Try Again!!!!");
+                    Console.Write("Side of Farmer: ");
+                    aux = Console.ReadLine();
+                }
+                state[i] = (aux == "R");
+            }
+
+            return state;
+        }
         static void Main(string[] args)
         {
             //each position of the vector represents the side of each item
@@ -22,13 +44,23 @@ namespace CrossingRiverBFS
              * if the value in the position is false, it means it is on the left side of the river
              * if it is true, it is on the right side
              * */
-            bool[] init = { false, false, false, false};
-            bool[] goal = { true, true, true, true};
+            Console.WriteLine("Use: L - Left R - Right");
+            Console.WriteLine("\nInform values of initial state");
+            bool[] init = ReadState();
 
+            while (GraphSearch.IsDeadState(init)) {
+                Console.WriteLine("\nInvalid State! Try again");
+                Console.WriteLine("\nInform values of initial state");
+                init = ReadState();
+            }
+            
+            Console.WriteLine("\nInform values of goal state");
+            bool[] goal = ReadState();
+            
             Node n1 = new Node(init);
             Node n2 = new Node(goal);
 
-            //Console.WriteLine(Node.ShowState(init));
+            Console.WriteLine("\n\n########## RESULT ###########\n");
             Path path = GraphSearch.BreadthFirstSearch(n1, n2);
             if (path != null)
             {
